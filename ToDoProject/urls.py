b28e0ab95ad11862227790daada83d1221cd6706
pai_puella <edyshkant-20@ranepa.ru>
2021-12-29 00:13:32 +0300

    Initial commit

---
+++ ToDoProject/urls.py
@@ -0,0 +1,11 @@
+"""ToDoProject URL Configuration
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from todoapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('todoapp.urls'))
+]
