d67e89e69b3c4e6670d8aaf613384f053bded82f
pai-puella <91475035+pai-puella@users.noreply.github.com>
2021-12-29 13:44:50 +0300

    Update urls.py

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('delete/<list_id>', views.delete, name='delete'),
-    path('cross_off/<list_id>', views.cross_off, name='cross_off'),
-    path('uncross/<list_id>', views.uncross, name='uncross'),
+    path('in_process/<list_id>', views.in_process, name='in_process'),
+    path('done/<list_id>', views.done, name='done'),
+    path('edit/<list_id>', views.edit, name='edit'),
 ]
